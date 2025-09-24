9f5c5765e1c0dad8a04f3a275b1408a47698937d
Norbinhoo <modinorbert14@gmail.com>
2025-09-24 12:38:36 +0300

    lab2

---
+++ lab02/Point.cpp
@@ -0,0 +1,61 @@
+#include "Point.h"
+#include <iostream>
+#include <valarray>
+#include <set>
+#include <fstream>
+
+using  namespace std;
+Point::Point(int x, int y) {
+    if (x >= 0 && x <= 2000 && y >= 0 && y <= 2000) {
+        this->x = x;
+        this->y = y;
+    }
+    else {
+        this->x = 0;
+        this->y = 0;
+    }
+}
+int Point::getX() const {
+    return x;
+}
+int Point::getY() const {
+    return y;
+}
+void Point::print() const{
+    cout<<x<<" "<<y<<endl;
+}
+
+double distance(const Point &a, const Point &b) {
+    return sqrt(pow(a.getX()-b.getX(),2)+pow(a.getY()-b.getY(),2));
+}
+
+bool isSquare(const Point &a, const Point &b, const Point &c, const Point &d) {
+    set<double> tavolsag;
+    tavolsag.insert(distance(a,b));
+    tavolsag.insert(distance(a,c));
+    tavolsag.insert(distance(a,d));
+    tavolsag.insert(distance(b,c));
+    tavolsag.insert(distance(b,d));
+    tavolsag.insert(distance(c,d));
+    if(tavolsag.size()==2){
+        return true;
+    }
+    return false;
+
+}
+void testIsSquare(const char * filename){
+    ifstream ifs(filename);
+    if(!ifs){
+        cout<<"File not found"<<endl;
+        exit(1);
+    }
+    int x,y;
+    Point points[100][100];
+    int index=0;
+    while(ifs>>x>>y){
+        points[index][]=Point(x,y);
+        ++index;
+    }
+
+}
+
--- lab02/main_02.cpp
+++ lab02/main_02.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include "Point.h"
+#include <fstream>
+using namespace std;
+int main(int argc, char** argv) {
+    Point p1(1,1);
+    cout<<"p1( "<<p1.getX()<<","<<p1.getY()<<")"<<endl;
+    Point p2(2, 2);
+    cout<<"p2( "<<p2.getX()<<","<<p2.getY()<<")"<<endl;
+    Point * pp1 = new Point(300, 400);
+    Point * pp2 = new Point(500, 1000);
+    cout<<"pp1( "<<pp1->getX()<<","<<pp1->getY()<<")"<<endl;
+    cout<<"pp2( "<<pp2->getX()<<","<<pp2->getY()<<")"<<endl;
+    pp1->print();
+    cout<<endl;
 
-int main() {
-    std::cout << "Hello, Lab_02" << std::endl;
+
+    cout<<distance(p1,p2)<<endl;
+
+    Point p3(1,1);
+    Point p4(2,2);
+
+    if(isSquare(p1,p2,p3,p4)==true){
+        cout<<"Negyzetet alkothat!";
+    }
+    else{
+        cout<<"Nem alkothat negyzetet!";
+    }
+
+    testIsSquare("data/input.txt");
+
+    delete pp1;
+    delete pp2;
+
+
     return 0;
 }
